91d6468dd4b48cf5fef639ce06ba20ca0d887255
Noha345 <blueworldbugs@gmail.com>
2026-01-22 08:30:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def hello_world():
+    return 'Universal downloader Bot is Running'
 
-if __name__ == '__main__':
-    # Run the Flask app on port 80
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    # Get the PORT from environment variables, default to 8080 if not found
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
