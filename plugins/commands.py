fdb809a5bfab125ee82c7a0644a5a48ebb9c7bdf
Noha345 <blueworldbugs@gmail.com>
2026-01-22 09:42:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,30 +1,44 @@
-import random
 import os
-import time
-import psutil
-import shutil
-import string
-import asyncio
-from asyncio import TimeoutError
-
-# Pyrogram
-from pyrogram import Client, filters, types, errors
-from pyrogram.types import (
-    InlineKeyboardButton, 
-    InlineKeyboardMarkup, 
-    Message, 
-    CallbackQuery, 
-    ForceReply
-)
-from pyrogram.errors import MessageNotModified
-
-# Local Plugins
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from plugins.config import Config
 from plugins.script import Translation
 from plugins.database.add import AddUser
-from plugins.database.database import db
 from plugins.functions.forcesub import handle_force_subscribe
-from plugins.settings.settings import OpenSettings
-from plugins.functions.verify import verify_user, check_token
 
-# --- Your Bot Logic Goes Below ---
+@Client.on_message(filters.command(["start"]) & filters.private)
+async def start(bot, update):
+    # 1. Add User to Database
+    await AddUser(bot, update)
+
+    # 2. Check Force Subscription (If enabled)
+    if Config.UPDATES_CHANNEL:
+        fsub = await handle_force_subscribe(bot, update)
+        # If user hasn't joined, fsub returns 400, so we stop here
+        if fsub == 400:
+            return
+
+    # 3. Send Start Message
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    )
+
+@Client.on_message(filters.command(["help"]) & filters.private)
+async def help_user(bot, update):
+    await AddUser(bot, update)
+    await update.reply_text(
+        text=Translation.HELP_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=Translation.HELP_BUTTONS
+    )
+
+@Client.on_message(filters.command(["about"]) & filters.private)
+async def about_user(bot, update):
+    await AddUser(bot, update)
+    await update.reply_text(
+        text=Translation.ABOUT_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=Translation.ABOUT_BUTTONS
+    )
